aa58b12a3403d8946af8dbecc605eb10c52a8e66
Rodrigo Matias <rodrigolzm@gmail.com>
2022-02-09 16:07:05 -0400

    added day 5 part 1

---
+++ src/main/java/day5/HydrothermalVenture.java
@@ -0,0 +1,42 @@
+package day5;
+
+public class HydrothermalVenture {
+
+	public static long partOne(int[][] input) {
+
+		var size = 0;
+
+		for (var line : input)
+			size = Math.max(size, Math.max(Math.max(line[0], line[1]), Math.max(line[2], line[3])));
+
+		var map = new int[size + 1][size + 1];
+
+		for (var line : input) {
+
+			if (line[1] == line[3]) {
+				for (var i = Math.min(line[0], line[2]); i <= Math.max(line[0], line[2]); i++) {
+					map[line[1]][i]++;
+				}
+			}
+
+			if (line[0] == line[2]) {
+				for (var i = Math.min(line[1], line[3]); i <= Math.max(line[1], line[3]); i++) {
+					map[i][line[0]]++;
+				}
+			}
+		}
+
+		var count = 0;
+
+		for (var row : map)
+			for (var point : row)
+				count += point < 2 ? 0 : 1;
+
+		return count;
+	}
+
+	public static long partTwo(Integer[][] input) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/day5/HydrothermalVentureTest.java
@@ -0,0 +1,35 @@
+package day5;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import common.LoadInput;
+
+public class HydrothermalVentureTest {
+
+	@Test
+	public void test1() {
+		var input = new int[][] { { 0, 9, 5, 9 }, { 8, 0, 0, 8 }, { 9, 4, 3, 4 }, { 2, 2, 2, 1 }, { 7, 0, 7, 4 },
+				{ 6, 4, 2, 0 }, { 0, 9, 2, 9 }, { 3, 4, 1, 4 }, { 0, 0, 8, 8 }, { 5, 5, 8, 2 } };
+		assertEquals(5, HydrothermalVenture.partOne(input));
+	}
+
+	@Test
+	public void test2() {
+		var lines = LoadInput.loadToString("day5/input.txt");
+
+		var input = new int[lines.size()][4];
+
+		for (int i = 0; i < lines.size(); i++) {
+			var parts = lines.get(i).split(" -> ");
+			var a = parts[0].split(",");
+			var b = parts[1].split(",");
+			input[i] = new int[] { Integer.valueOf(a[0]), Integer.valueOf(a[1]), Integer.valueOf(b[0]),
+					Integer.valueOf(b[1]) };
+		}
+		
+		assertEquals(5167, HydrothermalVenture.partOne(input));
+	}
+
+}
