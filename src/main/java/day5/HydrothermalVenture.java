cd0db51c000c7aca746c706eff88bb3cecef3480
Rodrigo Matias <rodrigolzm@gmail.com>
2022-02-12 15:01:40 -0400

    added day 5 part 2

--- src/main/java/day5/HydrothermalVenture.java
+++ src/main/java/day5/HydrothermalVenture.java
@@ -35,8 +35,47 @@ public class HydrothermalVenture {
 		return count;
 	}
 
-	public static long partTwo(Integer[][] input) {
-		return 0;
+	public static long partTwo(int[][] input) {
+		var size = 0;
+
+		for (var line : input)
+			size = Math.max(size, Math.max(Math.max(line[0], line[1]), Math.max(line[2], line[3])));
+
+		var map = new int[size + 1][size + 1];
+
+		for (var line : input) {
+			
+			if (line[1] == line[3]) {
+				
+				for (var i = Math.min(line[0], line[2]); i <= Math.max(line[0], line[2]); i++) {
+					map[line[1]][i]++;
+				}
+				
+			} else if (line[0] == line[2]) {
+				
+				for (var i = Math.min(line[1], line[3]); i <= Math.max(line[1], line[3]); i++) {
+					map[i][line[0]]++;
+				}
+
+			} else {
+				
+				var steps = Math.max(line[1], line[3]) - Math.min(line[1], line[3]);
+				
+				for (var i = 0; i <= steps; i++) {
+					var x = line[1] < line[3] ? line[1] + i : line[1] - i;
+					var y = line[0] < line[2] ? line[0] + i : line[0] - i;					
+					map[x][y]++;
+				}
+			}
+		}
+		
+		var count = 0;
+
+		for (var row : map)
+			for (var point : row)
+				count += point < 2 ? 0 : 1;
+
+		return count;
 	}
 
 }
--- src/test/java/day5/HydrothermalVentureTest.java
+++ src/test/java/day5/HydrothermalVentureTest.java
@@ -31,5 +31,29 @@ public class HydrothermalVentureTest {
 		
 		assertEquals(5167, HydrothermalVenture.partOne(input));
 	}
+	
+	@Test
+	public void test3() {
+		var input = new int[][] { { 0, 9, 5, 9 }, { 8, 0, 0, 8 }, { 9, 4, 3, 4 }, { 2, 2, 2, 1 }, { 7, 0, 7, 4 },
+				{ 6, 4, 2, 0 }, { 0, 9, 2, 9 }, { 3, 4, 1, 4 }, { 0, 0, 8, 8 }, { 5, 5, 8, 2 } };
+		assertEquals(12, HydrothermalVenture.partTwo(input));
+	}
+
+	@Test
+	public void test4() {
+		var lines = LoadInput.loadToString("day5/input.txt");
+
+		var input = new int[lines.size()][4];
+
+		for (int i = 0; i < lines.size(); i++) {
+			var parts = lines.get(i).split(" -> ");
+			var a = parts[0].split(",");
+			var b = parts[1].split(",");
+			input[i] = new int[] { Integer.valueOf(a[0]), Integer.valueOf(a[1]), Integer.valueOf(b[0]),
+					Integer.valueOf(b[1]) };
+		}
+		
+		assertEquals(17604, HydrothermalVenture.partTwo(input));
+	}
 
 }
